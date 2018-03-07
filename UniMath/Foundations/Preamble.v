3d02c4786ee3607ca5b6c20a06f8de2397b9df88
Daniel R. Grayson <dan@math.uiuc.edu>
2018-03-07 13:35:01 -0500

    update coq version to tip of SkySkimmer:univ-cumul

--- UniMath/Foundations/Preamble.v
+++ UniMath/Foundations/Preamble.v
@@ -22,7 +22,7 @@ Constraint uu0 < uu1, uu1 < uu2.
 Global Set Universe Polymorphism.
 Global Set Polymorphic Inductive Cumulativity.
 Global Unset Universe Minimization ToSet.
-Global Unset Cumulativity Use ULub.
+Global Unset Cumulativity Weak Constraints.
 
 Notation UU  := Type (only parsing).
 Notation UU0 := Type@{uu0} (only parsing).
